7de0a148cbf8ccb7ef408692cc9365cf348caab2
Alex Cruz <acruz@qualitrain.com.mx>
2024-06-16 14:23:32 -0600

    commit inicial
    plantilla com pom.xml creado por Spring Starter Project

---
+++ src/main/java/mx/com/qtx/demoMongo/DemoMongoDbSpringApplication.java
@@ -0,0 +1,13 @@
+package mx.com.qtx.demoMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMongoDbSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMongoDbSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/qtx/demoMongo/DemoMongoDbSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.qtx.demoMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMongoDbSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
